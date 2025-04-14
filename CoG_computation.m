52e3d3294540f2ef77555511f514911e886fe177
benediktfuhr <benedikt.fuhr@iit.it>
2025-04-14 13:18:54 +0200

    Add files via upload

--- CoG_computation.m
+++ CoG_computation.m
@@ -1,35 +1,31 @@
-% File paths
-atlas_path = '/Users/bened/Library/CloudStorage/OneDrive-FondazioneIstitutoItalianoTecnologia/Desktop/University/PhD_IIT/Turbulence_Autisms/Turbulence_Project/data/postproc/Files/schaefer_1054/SchaeferTian.nii';
+% Read the parcellation 
+parc_img = niftiread('/Users/bened/Library/CloudStorage/OneDrive-FondazioneIstitutoItalianoTecnologia/Desktop/University/PhD_IIT/Turbulence_Autisms/Schaefer_Parcellation/Schaefer2018_1000Parcels_7Networks_order_FSLMNI152_2mm.nii');
+donders_img = niftiread('/Users/bened/Library/CloudStorage/OneDrive-FondazioneIstitutoItalianoTecnologia/Desktop/University/PhD_IIT/Turbulence_Autisms/Turbulence_Project/data/postproc/Files/Schaefer1054_7networks_2mm.nii');
+% Header info needed later for the storage of the new parcellation file
+parc_info = niftiinfo('/Users/bened/Library/CloudStorage/OneDrive-FondazioneIstitutoItalianoTecnologia/Desktop/University/PhD_IIT/Turbulence_Autisms/Schaefer_Parcellation/Schaefer2018_1000Parcels_7Networks_order_FSLMNI152_2mm.nii');
 
-% Load the files and data
-atlas_file = spm_vol(atlas_path);
-atlas_data = spm_read_vols(atlas_file);
-
-% Initialize a 3D matrix to store the coordinates
-coords_3D = NaN * ones([size(atlas_data), 3]);
-
-% Get the size of the atlas
-[dim_x, dim_y, dim_z] = size(atlas_data);
-
-% Loop over all voxels in the atlas and get the coordinates
-for x = 1:dim_x
-    for y = 1:dim_y
-        for z = 1:dim_z
-            % If the voxel is non-zero, get its coordinates
-            % Convert voxel (x, y, z) to coordinates
-            real_world_coords = atlas_file.mat * [x; y; z; 1]; % Homogeneous coordinates
-            coords_3D(x, y, z, :) = real_world_coords(1:3); % Store x, y, z in the 4th dimension
-        end
-    end
+% Convert parc_img to match the datatype of the original NIfTI
+switch parc_info.Datatype
+    case 'uint8'
+        parc_img = uint8(parc_img);
+    case 'int16'
+        parc_img = int16(parc_img);
+    case 'int32'
+        parc_img = int32(parc_img);
+    case 'single'
+        parc_img = single(parc_img);
+    case 'double'
+        parc_img = double(parc_img);
+    otherwise
+        error('Unsupported NIfTI data type: %s', nifti_info.Datatype);
 end
 
-
-%Compute CoG
-
-CoG_54 = ones(1054,3);
-for parc = 1:1054
-    curr_Mask = atlas_data == parc;
-    curr_voxel = coords_3D(repmat(curr_Mask, [1 1 1 3]));
-    curr_coords = reshape(curr_voxel,[],3);
-    CoG_54(parc,:) = mean(curr_coords,1);
+% Modify the parcellation based on region IDs from donders_parc (1001:1054)
+for i = 1001:1054
+    mask = ismember(donders_img, i);  % Create a mask for each region
+    parc_img(mask) = i;  % Modify parc_img with the region IDs from donders_parc
 end
+
+% Save the modified parcellation back with correct metadata
+parc_info.Filename = 'Schaefer1054_7networks_2mm.nii.gz';  % Set output filename
+niftiwrite(parc_img, 'Schaefer1054_7networks_2mm.nii.gz', parc_info, 'Compressed', false);
